2aa26ad59b20e45ba499cf0de2d9f010c0450154
Globox1997 <Micha.Damaschke@web.de>
2026-02-26 09:34:32 +0100

    added show root option

--- src/main/java/net/questz/config/QuestConfig.java
+++ src/main/java/net/questz/config/QuestConfig.java
@@ -11,11 +11,11 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config.Gui.Background("minecraft:textures/block/stone.png")
 public class QuestConfig implements ConfigData {
 
-    public double test = 1.0D;
     public int maxTextWidth = 180;
     public int minTextWidth = 150;
     @Comment("How many ongoing quests on one branch will get shown")
     public int questDisplayDepth = 18;
+    public boolean showRootRequirements = false;
     @Comment("namespace ids of advancements only shown in the quest screen")
     public ArrayList<String> questAdvancementNamespaceIds = new ArrayList<>(List.of("questz"));
 
